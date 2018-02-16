aab21c973be0cd15227f701a21a4d3fcd1b13e9c
Rahul Patil <rahulpatil2387@gmail.com>
2018-02-17 00:09:58 +0530

    Add code to convert money to proper formatted currency

--- currency_conversion.rb
+++ currency_conversion.rb
@@ -1,5 +1,74 @@
+## Main file responsible for taking user input amount,
+# processing converting it into prper format and showing desired output
 class CurrencyConversion
- def change_for
-    "Inside change_for method!!"
- end
+
+  #Taking inputs from user
+  def get_user_inputs
+    more = true
+    while more==true
+      puts "Enter non-negetive amount!"
+      amount = gets.chomp
+      begin
+        Integer(amount)
+        if amount.to_i.positive?
+          puts currency_mapping_hash.merge(change_for(amount.to_i))
+        else
+          puts 'Please enter non-negetive amount!'
+        end
+      rescue ArgumentError, TypeError
+        puts 'Not a number! Please enter non-negetive amount!'
+      end
+      #User have to enter y if he want to run it again
+      puts "Do you want to enter more y/n?"
+      a = gets.chomp
+      if a!="y"
+        #if user enters other than 'y' then making 'more' to 'False' to stop the loop. As condition in while will not be satisfied then
+        more = false
+      end
+    end
+  end
+
+  # basic
+   def change_for_basic(amount)
+      qq, dm = amount.divmod(25)
+      dm, pp = dm.divmod(10)
+      nn, pp = pp.divmod(5)
+      puts "%d Quarters, %d Dimes, %d Nickels and %d Pennies" % [qq, dm, nn, pp]
+   end
+
+  # recursive
+   def change_for(amount, result={})
+      return {} if amount == 0
+      coin_name, divisor = get_divisor(amount)
+      quotient, modulus = amount.divmod(divisor)
+      result[coin_name] = quotient
+      change_for(modulus, result) if quotient > 0
+      result
+   end
+
+  private
+   def get_divisor(divisor)
+    case divisor
+    when  1..4
+      return 'Pennies', 1
+    when 5..9
+      return 'Nickels', 5
+    when 10..24
+      return 'Dimes', 10
+    else
+      return 'Quarters', 25
+    end
+   end
+
+   def currency_mapping_hash
+    {
+      'Quarters' => 0,
+      'Dimes' => 0,
+      'Nickels' => 0,
+      'Pennies' => 0
+      }
+   end
 end
+
+cc = CurrencyConversion.new
+cc.get_user_inputs
--- spec/currency_conversion_spec.rb
+++ spec/currency_conversion_spec.rb
@@ -1,12 +1,46 @@
 require "spec_helper"
+require 'stringio'
 
-describe CurrencyConversion do
-   context "#change_for" do
+def get_user_inputs
+  $stdin.gets.chomp
+end
 
-      it "should say 'Hello World' when we call the say_hello method" do
-         cc = CurrencyConversion.new
-         message = cc.change_for
-         expect(message).to eq "Inside change_for method!!"
-      end
-   end
+describe 'CurrencyConversion' do
+  context '#get_user_inputs' do
+    before do
+      $stdin = StringIO.new("41\n")
+    end
+
+    after do
+      $stdin = STDIN
+    end
+
+    it "should get user inputs" do
+      expect(get_user_inputs).to be == '41'
+    end
+  end
+
+  context '#change_for_basic' do
+    before do
+      @cc = CurrencyConversion.new
+    end
+
+    it "should be amount to convert to desired currency format" do
+      expect { @cc.change_for_basic(25) }.to output("1 Quarters, 0 Dimes, 0 Nickels and 0 Pennies\n").to_stdout
+    end
+
+    it "should be amount to convert to desired currency format" do
+      expect { @cc.change_for_basic(41) }.to output("1 Quarters, 1 Dimes, 1 Nickels and 1 Pennies\n").to_stdout
+    end
+  end
+
+  context '#change_for' do
+    before do
+      @cc = CurrencyConversion.new
+    end
+
+    it "should be amount to convert to desired currency format" do
+      expect { puts @cc.change_for(41) }.to output(/{"Quarters"=>1, "Dimes"=>1, "Nickels"=>1, "Pennies"=>1}/).to_stdout
+    end
+  end
 end
